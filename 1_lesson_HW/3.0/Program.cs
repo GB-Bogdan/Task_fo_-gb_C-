b73f97c4ca8cac43578ec7b36e41acb1cea0f011
Bogdan <akinfeeff.bogdan@gmail.com>
2022-11-26 18:07:58 +0300

    3_HW

---
+++ 1_lesson_HW/3.0/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Введите число: ");
+int number = int.Parse(Console.ReadLine());
+if (number % 2==1)
+{
+    Console.WriteLine("Число нечетное");
+}
+else
+{
+    Console.WriteLine("Число четное");
+}
