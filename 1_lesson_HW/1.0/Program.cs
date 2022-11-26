3e722b3de56fd5d7bbd6533f6b4c26203aa60b8e
Bogdan <akinfeeff.bogdan@gmail.com>
2022-11-26 17:15:08 +0300

    1_HW

--- 1_lesson_HW/1.0/Program.cs
+++ 1_lesson_HW/1.0/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите первое число: ");
+Double a = Double.Parse(Console.ReadLine());
+Console.WriteLine("Введите второе число: ");
+Double b = Double.Parse(Console.ReadLine());
+if (a > b)
+{
+    Console.WriteLine("Число a больше числа b");
+}
+else if (a < b)
+{
+    Console.WriteLine("Число a меньше числа b");
+}
+else
+{
+    Console.WriteLine("Число a равно числу b");
+}
