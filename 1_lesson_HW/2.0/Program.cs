2c4615ee7d1d07164915540f235853b6ecc0c7a9
Bogdan <akinfeeff.bogdan@gmail.com>
2022-11-26 17:52:26 +0300

    2_HW

---
+++ 1_lesson_HW/2.0/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.WriteLine("Введите первое число: ");
+Double a = Double.Parse(Console.ReadLine());
+Console.WriteLine("Введите второе число: ");
+Double b = Double.Parse(Console.ReadLine());
+Console.WriteLine("Введите третье число: ");
+Double c = Double.Parse(Console.ReadLine());
+Double max = 0;
+if (a > max) 
+{
+    max = a;
+}
+if (b > max)
+{
+    max = b;
+}
+if (c > max)
+{
+    max = c;
+}
+Console.WriteLine("Максимальное число");
+Console.Write(max);
