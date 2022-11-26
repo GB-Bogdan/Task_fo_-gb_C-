4a69ea27dab7ae73a465679656a19020850ffea5
Bogdan <akinfeeff.bogdan@gmail.com>
2022-11-26 18:40:18 +0300

    4_HW

---
+++ 1_lesson_HW/4.0/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Введите количество чисел: ");
+int N = int.Parse(Console.ReadLine());
+int m = 1;
+Console.WriteLine("Четные числа: ");
+while (m <= N)
+    {
+        if (m % 2==1)
+        {
+            m++;
+        }
+        else
+        {
+            Console.WriteLine(m);   
+            m++;
+        }
+    }
