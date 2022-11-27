1fffa5292210622b9fb8b13d7979ecf81e0c22ac
Bogdan <akinfeeff.bogdan@gmail.com>
2022-11-27 12:16:33 +0300

    2_HW

---
+++ 2_lesson_HW/2.0/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Введите трехзначное число: ");
+int number = int.Parse(Console.ReadLine());
+int second = 0;
+int number1 = number/100;
+if (number > 1000)
+{
+    Console.WriteLine("Некорректное значение");
+}
+else 
+{
+    second = number1*10 + number % 10;
+    Console.WriteLine(second);
+}
