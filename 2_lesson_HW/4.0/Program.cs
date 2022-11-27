79d5e73120caf93a462448057d8702bed425345f
Bogdan <akinfeeff.bogdan@gmail.com>
2022-11-27 13:16:19 +0300

    4_HW

---
+++ 2_lesson_HW/4.0/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Введите цифру, обозначающую день недели: ");
+int daynum = int.Parse(Console.ReadLine());
+if (daynum == 6 || daynum == 7)
+{
+    Console.WriteLine("Этот день выходной");
+}
+else if (daynum < 1 || daynum > 7)
+{
+    Console.WriteLine("Дней недели 7");
+}
+  else
+{
+    Console.WriteLine("Этот день не выходной");
+}
