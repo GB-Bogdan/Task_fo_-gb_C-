34b6fb9843742cd49fdeffeb8533fe3f2f7f273c
Bogdan <akinfeeff.bogdan@gmail.com>
2022-11-27 12:46:27 +0300

    3_HW

---
+++ 2_lesson_HW/3.0/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Введите число: ");
+int number = int.Parse(Console.ReadLine());
+int third = 0;
+if (number >= 100)
+{
+    while(number > 1000)
+    {
+        number = number/10;
+    }
+    third = number % 10;
+    Console.WriteLine(third);
+}
+else 
+{
+    Console.WriteLine("Третьей цифры нет");
+}
