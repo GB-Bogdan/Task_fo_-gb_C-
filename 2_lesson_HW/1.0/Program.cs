965a2a2691664c1b059ff2ff04375a37bfec0cff
Bogdan <akinfeeff.bogdan@gmail.com>
2022-11-27 12:02:42 +0300

    1_HW

--- 2_lesson_HW/1.0/Program.cs
+++ 2_lesson_HW/1.0/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите трехзначное число: ");
+int number = int.Parse(Console.ReadLine());
+int second = 0;
+if (number > 1000)
+{
+    Console.WriteLine("Некорректное значение");
+}
+else 
+{
+    second = number/10 % 10;
+    Console.WriteLine(second);
+}
