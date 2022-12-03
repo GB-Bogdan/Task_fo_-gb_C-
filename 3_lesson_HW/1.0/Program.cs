d255d01778ca5587267d3d60af99d7450625757c
Bogdan <akinfeeff.bogdan@gmail.com>
2022-12-03 12:42:47 +0300

    1_hw

---
+++ 3_lesson_HW/1.0/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("Введите пятизначное число: ");
+string number = Console.ReadLine();
+
+if (number!.Length == 5)
+{
+    CheckingNumber(number);
+}
+else Console.WriteLine("Введи правильное число");
+
+void CheckingNumber(string number)
+{
+    if (number[0]==number[4] & number[1]==number[3])
+    {
+        Console.WriteLine("Это чило палиндром");
+    }
+    else Console.WriteLine("Это чило не палиндром");
+}
