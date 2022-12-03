877945256eaf72eb71c555d6730ffd3141f12c86
Bogdan <akinfeeff.bogdan@gmail.com>
2022-12-03 12:36:44 +0300

    1_hw

--- 3_lesson_HW/Program.cs
+++ 3_lesson_HW/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите пятизначное число: ");
+string number = Console.ReadLine();
+
+if (number!.Length == 5)
+{
+    CheckingNumber(number);
+}
+else Console.WriteLine("Введи правильное число");
+
+void CheckingNumber(string number)
+{
+    if (number[0]==number[4] & number[1]==number[3])
+    {
+        Console.WriteLine("Это чило палиндром");
+    }
+    else Console.WriteLine("Это чило не палиндром");
+}
