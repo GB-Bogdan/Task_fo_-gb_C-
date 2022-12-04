c98c6dc6634fe65e8e2aae378ed13b6524351af3
Bogdan <akinfeeff.bogdan@gmail.com>
2022-12-04 18:12:55 +0300

    1_hw

---
+++ 4_lesson_HW/1.0/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("Введите число A: ");
+int A = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите число B: ");
+int B = int.Parse(Console.ReadLine());
+
+int value(int A, int B)
+{
+    int result = 1;
+  for(int i=1; i <= B; i++)
+  {
+    result = result * A;
+  }
+    return result;
+}
+
+int Value = value(A, B);
+Console.WriteLine(Value);
