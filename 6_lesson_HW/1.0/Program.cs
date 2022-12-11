11834c3f73717d516d836a58acc8f6b87b7aee77
Bogdan <akinfeeff.bogdan@gmail.com>
2022-12-11 20:10:35 +0300

    1_HW

---
+++ 6_lesson_HW/1.0/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите числа: ");
+double[] arr = Array.ConvertAll(Console.ReadLine().Split(), double.Parse);
+int count = 0;
+
+for (int i = 0; i < arr.Length; i++)
+{
+    if (arr[i] > 0)
+    {
+        count++;
+    }
+}
+Console.WriteLine(count);
